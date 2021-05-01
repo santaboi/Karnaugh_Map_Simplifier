98cf6ff683c49be0a7e9152cad30eea129966e6b
Yang Fang-Jiang <76427253+santaboi@users.noreply.github.com>
2021-05-01 11:45:12 +0800

    Add files via upload

--- K_mapNew.cpp
+++ K_mapNew.cpp
@@ -11,18 +11,6 @@ void Switch_Judge( char  to_judge);
 inline void reset();
 int A_signal=-1,B_signal=-1,C_signal=-1,D_signal=-1;
 void KMAP_Changer(int,int,int,int);
-int one_num=0;
-//***********************
-struct minterm
-{
-    int binary[4];
-    int status=(-1);
-};
-/*
-for(int i=0;i<one_num;i++)
-    {   cur_ptr[i].status=(-1);}
-*/
-//*************************
 
 int K_MAP[4][4]={0};//initialize all 0
 
@@ -41,209 +29,18 @@ int main()
     {   break;}
     }
     KMAP_Changer(A_signal,B_signal,C_signal,D_signal); //for last patern
-    //cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;//debug use
+    cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;
     reset();   
-    //cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;//debug use
-    
+    cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;
 
-     cout<<"===========K Kap=========="<<endl;
-    cout<<"  \\AB|                   "<<endl;
-    cout<<"CD\\  | 00 | 01 | 11 | 10 "<<endl;
-     cout<<"-----+----+----+----+-----"<<endl;
+   
     for(int i=0;i<4;i++)
     {
-        cout<<"     |    |    |    |    |"<<endl;
-        switch(i)
-            {
-                case 0:
-                cout<<"  00 |";
-                break;
-                case 1:
-                cout<<"  01 |";
-                break;
-                case 2:
-                cout<<"  11 |";
-                break;
-                case 3:
-                cout<<"  10 |";
-                break;
-            }
         for(int j=0;j<4;j++)
-        {
-            cout<<"  "<<K_MAP[i][j]<<" |";
-            if(K_MAP[i][j]==1)
-            one_num++;
-        }
-        cout<<endl<<"     |    |    |    |    |"<<endl;
-        cout<<"-----+----+----+----+-----"<<endl;
+        {   cout<<K_MAP[i][j]<<' ';}
+        cout<<endl;
     }
 
-    //one_num=minterm number
-struct minterm *cur_ptr=new struct minterm[one_num];
-struct minterm *new_ptr=new struct minterm[one_num];//比較完存現有值
-    int index=0;
-    for(int j=0;j<4;j++) 
-    {
-        for(int i=0;i<4;i++)
-        {
-            if(K_MAP[i][j]==1)
-            {   
-                
-                switch (j)
-                {
-                case 0://first column
-                    if(i==0)//0000
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={0};}
-                    if(i==1)//0001
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==2)//0011
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==3)//0010
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={0};}
-                    break;
-                case 1://second column
-                    if(i==0)//0100
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={0};}
-                    if(i==1)//0101
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==2)//0111
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==3)//0110
-                    {cur_ptr[index].binary[0]={0};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={0};}
-                    break;
-                case 2://third column
-                    if(i==0)//1100
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={0};}
-                    if(i==1)//1101
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==2)//1111
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==3)//1110
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={1};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={0};}
-                    break;
-                case 3://fourth column
-                    if(i==0)//1000
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={0};}
-                    if(i==1)//1001
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={0};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==2)//1011
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={1};}
-                    if(i==3)//1010
-                    {cur_ptr[index].binary[0]={1};
-                    cur_ptr[index].binary[1]={0};
-                    cur_ptr[index].binary[2]={1};
-                    cur_ptr[index].binary[3]={0};}
-                    break;
-                default:
-                    break;
-                }    
-
-                index++;
-            }
-        }
-    }
-   //cur_ptr[one_number]裡面已經建立完所有patern 的binary數值
-
-//**********************************queen macusky simplification************************************************************
-    int new_ind=0;
-    for(int i=0;i<(one_num-1);i++)
-    {
-        for(int j=i+1;j<one_num;j++)
-        {
-            for(int ind=0;ind<4;ind++)//differ是一組ind(0123)共用的值
-            {
-                int differ=0;
-                if(cur_ptr[i].binary[ind]==cur_ptr[j].binary[ind])
-                {   
-                    new_ptr[new_ind].binary[ind]=cur_ptr[i].binary[ind];
-                }
-                else if(cur_ptr[i].binary[ind]!=cur_ptr[j].binary[ind])//不一樣就改(-1)不論幾個
-                {
-                    new_ptr[new_ind].binary[ind]=(-1);
-                    differ++;
-                }
-
-                if((ind==3)&&(differ==1))//ij merge successfully (if failed ,new_ind沒++ 下一輪會把不要的洗掉)
-                {   
-                    cur_ptr[i].status=1;
-                    cur_ptr[j].status=1;
-                    new_ind++;
-                }
-                
-            }
-        }
-    }
-    for(int i=0;i<one_num;i++)//check status
-    {   if(cur_ptr[i].status=(-1))
-        {   new_ptr[new_ind].binary[0]=cur_ptr[i].binary[0];
-            new_ptr[new_ind].binary[1]=cur_ptr[i].binary[1];
-            new_ptr[new_ind].binary[2]=cur_ptr[i].binary[2];
-            new_ptr[new_ind].binary[3]=cur_ptr[i].binary[3];
-            new_ind++;
-        }
-    }
-    //status 要reset嗎
-    
-    for(int i=0;i<one_num;i++)
-    {   
-        cout<<cur_ptr[i].binary[0]<<cur_ptr[i].binary[1]<<cur_ptr[i].binary[2]<<cur_ptr[i].binary[3]<<' ';
-    }
-    cout<<endl;
-    for(int i=0;i<new_ind;i++)
-    {   cout<<new_ptr[i].binary[0]<<new_ptr[i].binary[1]<<new_ptr[i].binary[2]<<new_ptr[i].binary[3]<<' ';}
-    cout<<endl;
-    //一圈完成
-    //new_ind 是正確element的index +1
-    //取正確的值只取長度為new_ind的(捨去最後一個)
-        
-//******************************************************************************
-    delete [] cur_ptr;
-    delete[] new_ptr;
     system("PAUSE");
     return 0;
 }
@@ -265,33 +62,27 @@ case 'a':
     break;
 case 'b':
     B_signal=1;
-    if(infile>>judge)
-    {
+    infile>>judge;
     if(judge=='\'')
     {B_signal=0;}
     else
     {Switch_Judge(judge);}//recursive
-    }
     break;
 case 'c':
     C_signal=1;
-    if(infile>>judge)
-    {
+    infile>>judge;
     if(judge=='\'')
     {C_signal=0;}
     else
     {Switch_Judge(judge);}//recursive
-    }
     break;
 case 'd':
     D_signal=1;
-    if(infile>>judge)
-    {
+    infile>>judge;
     if(judge=='\'')
     {D_signal=0;}
     else
     {Switch_Judge(judge);}//recursive
-    }
     break;
 case '('://don't care
     break;
@@ -301,12 +92,13 @@ case '\''://the form of char 'prime' is \'
     break;
 case '+':
     KMAP_Changer(A_signal,B_signal,C_signal,D_signal);
-    //cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;//debug use
+    //test
+    cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;
     reset();
-    //cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;//debug use
+    //test
+    cout<<A_signal<<B_signal<<C_signal<<D_signal<<endl;
     
     break;
-
 default:
     cout<<"input format error!";
     break;
